69feb8f603ee748cb19f061248d2e2e30e99a2b1
ruben <rubenmartinezlozano@gmail.com>
2020-11-13 12:12:57 +0100

    Modificación después de la entrega

--- main.js
+++ main.js
@@ -1,37 +1,50 @@
-document.title = 'Ahora trabajando con DOM dinámico';
+const ej1 = () => {
+    document.title = 'Ahora trabajando con DOM dinámico';
+}
 
-let secciones = document.getElementsByTagName('section');
-[...secciones].forEach((elem, index) => elem.id = "seccion_" + index);
+const ej2 = () => {
+    let secciones = document.getElementsByTagName('section');
+    [...secciones].forEach((elem, index) => elem.id = "seccion_" + index);
+}
 
-let p = document.getElementsByTagName('p');
-[...p].forEach(elem => elem.style.fontSize = "7");
+const ej3 = () => {
+    let p = document.getElementsByTagName('p');
+    [...p].forEach(elem => elem.style.fontSize = 22);
+}
 
+const ej4 = () => {
+    let enlace = document.getElementsByTagName('a');
+    [...enlace].forEach(e => {
+        e.addEventListener('focus', () => {
+            if (!e.focus) {
+                e.style = "";
+                e.classList = "";
+            }
+        });
+    });
+}
 
-let enlace = document.getElementsByTagName('a');
-[...enlace].forEach(e => {
-    e.addEventListener('focus', () => {
-        if (!e.focus) {
-            e.style = {};
-            e.classList = "";
+const ej5 = () => {
+    let check = document.createElement('input');
+    check.type = "checkbox";
+    check.value = "Cambiar imagen";
+    let boton = document.getElementById('boton');
+    check.addEventListener('change', () => {
+        check.checked ? boton.disabled = false : boton.disabled = true;
+    });
+    let foto = document.getElementById('foto');
+    //document.insertBefore(check);
+
+    boton.addEventListener('click', () => {
+        if (!check.disabled) {
+            check.disabled = true;
+            foto.src = "https://i0.wp.com/www.silocreativo.com/wp-content/uploads/2015/05/logotipo-google.jpg?w=666&quality=100&strip=all&ssl=1";
         }
-    })
-})
+    });
+}
 
-
-let check = document.createElement('input');
-check.type = "checkbox";
-check.value = "Cambiar imagen";
-let boton = document.getElementById('boton');
-check.addEventListener('change', () => {
-    check.checked ? boton.disabled = false : boton.disabled = true;
-});
-let foto = document.getElementById('foto');
-foto.insertAdjacentElement(beforeend, check);
-
-
-boton.addEventListener('click', () => {
-    if (!check.disabled) {
-        check.disabled = true
-        foto.src = "https://i0.wp.com/www.silocreativo.com/wp-content/uploads/2015/05/logotipo-google.jpg?w=666&quality=100&strip=all&ssl=1";
-    }
-})
+ej1();
+ej2();
+ej3();
+ej4();
+ej5();
